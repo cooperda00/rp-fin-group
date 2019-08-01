aa30bcaca274139c31f60e9bfb3479cbda249700
Dan Cooper <cooperda.5@gmail.com>
2019-08-01 11:20:08 +0700

    twitter card

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `RP Financial Group: A trusted financial consultancy and private brokerage offering investment and financial planning solutions to individuals and businesses throughout Asia Pacific`,
     author: `danielcooper.io`,
     siteUrl: "https://www.rpfingroup.com",
-    image: "/images/logov3.png",
+    image: "/images/twittercard.png",
   },
   plugins: [
     `gatsby-plugin-sass`,
